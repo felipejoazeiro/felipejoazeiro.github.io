4436e047c184f5b12383f96c059e0b01d710c3fb
Felipe Joazeiro <felipejoazeiro@gmail.com>
2021-07-01 23:50:47 -0300

    start of @media

--- js/index.js
+++ js/index.js
@@ -23,3 +23,21 @@ function linkAction(){
 }
 
 navLink.forEach(n=>n.addEventListener('click', linkAction))
+
+const skillsContent = document.getElementsByClassName('skills__content')
+const skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i=0; i<skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass == 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((e)=>{
+    e.addEventListener('click', toggleSkills)
+})
